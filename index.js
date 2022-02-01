8dc9634d8c3a4ee50aee98d81e5f43729a484fb2
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-01 12:49:41 +0000

    add some brainstorming comments in index.js. Will delete later.

--- index.js
+++ index.js
@@ -44,8 +44,30 @@ app.get("/search/:query", async (req, res) => {
     const { query } = req.params;
     console.log(`query is: ${query}`)
 
-    const dataFromPython = await get_movieIds_from_query(query);
-    res.send(dataFromPython);
+    //Get movie ids matching the query
+    const movie_ids_matching_query = await get_movieIds_from_query(query);
+    console.log(movie_ids_matching_query);
+
+    //Get movie ids for recommended movies
+
+
+    //Render all movie_ids into JSON movie objects by querying database
+
+    //Send JSON movie objects to client side.
+
+    //Option 1) Choose the movie which matches the query the most?
+    //Option 2) Don't choose any movie, send all movies received to user
+
+    //We will need also to get a list of recommendations. 
+    //but will this be for a single movie, or for a whole list of movies?
+    //it is easier to do with just a single movie. But if the python algorithm only performs recommendations
+    //for one movie, how do we choose this movie? The one who looks the most like the user query.
+    //Is this done in the python script, and then sent as a JSON object, {primary_movie: 123, others: [1,2,..,n]}
+    //or in this Express server?
+    //It would make more sense to do it in the python script, as that's where all the data processing is taking place.
+    //thus, the Express server would only render the actual objects to send to the client side, given movie ids.
+
+
 });
 
 app.get('/recommendations', (req, res) => { })
