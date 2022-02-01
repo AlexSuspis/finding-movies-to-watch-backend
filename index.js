d43ce56a9740a9683453ee8eb5d0d87a06b1a8a9
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-01 12:17:40 +0000

    fix promise mechanism between server and python script. The server is properly waiting for the async request to finish.

--- index.js
+++ index.js
@@ -27,11 +27,11 @@ app.get('/python', (req, res) => {
 })
 
 
-const pythonPromise = () => {
+const get_movieIds_from_query = (query) => {
     return new Promise((resolve, reject) => {
         try {
 
-            const python = spawn("python", ["./recommendation-system/app.py", 'Superman']);
+            const python = spawn("python", ["./recommendation-system/app.py", query]);
 
             python.stdout.on("data", (data) => {
                 resolve(data.toString());
@@ -47,41 +47,47 @@ const pythonPromise = () => {
     });
 };
 
-app.get("/test-python", async (req, res) => {
-    const dataFromPython = await pythonPromise();
-    res.send(dataFromPython);
-});
-
-
-
-const get_movies_from_query = (query => {
-    const pythonProcess = spawn('python', ["recommendation-system/app.py", query]);
-
-    results = [];
-
-    pythonProcess.stdout.on('data', (data) => {
-        //console.log(Buffer.from(data).values())
-        results.push(data);
-        // console.log(data.toString())
-    });
-
-    pythonProcess.stdout.on('end', () => {
-        console.log("python script ended")
-        // let resultData = Buffer.from(results).values()
-        //let resultData = JSON.parse(results)
-        let resultData = results.toString()
-        console.log("results: ", resultData)
-    })
-})
-
-app.get('/search/:query', (req, res) => {
+app.get("/search/:query", async (req, res) => {
     const { query } = req.params;
     console.log(`query is: ${query}`)
 
-    movie_ids = get_movies_from_query(query)
-    //console.log(movie_ids)
+    const dataFromPython = await get_movieIds_from_query(query);
+    res.send(dataFromPython);
+});
 
-})
+// async function get_movies_from_query(query) {
+//     try {
+
+//         const pythonProcess = spawn('python', ["recommendation-system/app.py", query]);
+
+//         results = [];
+
+//         pythonProcess.stdout.on('data', (data) => {
+//             results.push(data);
+//         });
+
+//         await pythonProcess.stdout.on('end', () => {
+//             let resultData = results.toString()
+//             console.log("results: ", resultData)
+//             return resultData
+//         })
+
+//     } catch (err) {
+//         console.log("error in calling python script")
+//     }
+// }
+
+// app.get('/search/:query', async (req, res) => {
+
+//     const { query } = req.params;
+//     console.log(`query is: ${query}`)
+
+//     const results = await get_movies_from_query(query)
+//     // get_movies_from_query(query)
+//     //     .then(data => console.log("successful resolved promise! results are: ", data))
+//     //     .catch(err => console.log('caught an error calling get_movies_from_query()!: ', err))
+//     console.log("this should only run after we get results!", results)
+// })
 
 app.get('/recommendations', (req, res) => { })
 
