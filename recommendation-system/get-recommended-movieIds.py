870931b880228c8072fa529c6aa555f392417091
Alex Suspiro <alexandresuspiro@gmail.com>
2022-02-15 12:51:04 +0000

    implement knn classification for similar movies

--- recommendation-system/get-recommended-movieIds.py
+++ recommendation-system/get-recommended-movieIds.py
@@ -17,45 +17,52 @@ else:
 
 	#Load recommendation model
 	import pickle
+
+	# filename = 'knn_model.sav'
 	filename = 'recommendation-system/knn_model.sav'
 	knn_model = pickle.load(open(filename, 'rb'))
 	# print(knn_model)
 
+	# final_df = pd.read_csv('final_df.csv')
 	final_df = pd.read_csv('recommendation-system/final_df.csv')
 	final_df.set_index('movieId', inplace=True)
 	# print(final_df)
-	movie_row = final_df.loc[movieId]
-	# print(movie_row)
+	# print(final_df)
+	try:
+		movie_row = final_df.loc[movieId]
 
-	distances, indices = knn_model.kneighbors(movie_row.values.reshape(1,-1))
-	# print()
-	# print('distances: ', distances) 
-	# print('indices: ', indices)
+		distances, indices = knn_model.kneighbors(movie_row.values.reshape(1,-1))
+		# print()
+		# print('distances: ', distances) 
+		# print('indices: ', indices)
 
-	#get movieIds from indices
-	movieIds = []
-	for idx in indices[0]:
-		# print(final_df.iloc[idx])
-		movieIds.append(int(final_df.index[idx]))
+		#get movieIds from indices
+		movieIds = []
+		for idx in indices[0]:
+			# print(final_df.iloc[idx])
+			movieIds.append(int(final_df.index[idx]))
 
-	#remove initial movieId from movieIds list
-	movieIds.remove(movieId)
+		#remove initial movieId from movieIds list
+		movieIds.remove(movieId)
 
-	# print()
-	# print(movieIds)
-	# print(type(movieIds))
-
-
-	response = json.dumps((movieIds))
-	print(response)
-
-	#Predict recommended films
-		#Get 10 most similar films 
-		#Create an array
-		#JSON.dump(array)
-		#print(array)
+		# print()
+		# print(movieIds)
+		# print(type(movieIds))
 
 
+		response = json.dumps((movieIds))
+		print(response)
+
+		#Predict recommended films
+			#Get 10 most similar films 
+			#Create an array
+			#JSON.dump(array)
+			#print(array)
+		# print(movie_row)
+
+	except:
+		print([])
+		
 
 
 
