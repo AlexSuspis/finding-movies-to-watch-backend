a94b57a0bafb185376718eb4d68a8df0440bf600
Alex Suspiro <alexandresuspiro@gmail.com>
2022-03-04 14:46:35 +0000

    change function name in recommendation_system in order to use different recommendation functions.

--- recommendation-system/recommendation_system.py
+++ recommendation-system/recommendation_system.py
@@ -4,9 +4,7 @@ import json
 import numpy as np
 import utils
 
-
-
-def get_recommendations_for_movieId(movieId, n_recommendations):
+def predict_similairity_matrix(movieId, n_recommendations):
 	similarity_matrix = loader.load_similarity_matrix_locally()
 	# print(similarity_matrix)
 
@@ -17,18 +15,12 @@ def get_recommendations_for_movieId(movieId, n_recommendations):
 	recommended_movieIds = sorted_row[:n_recommendations].index
 	# print(recommended_movieIds)
 
-	#find indices of top 10 highest values in row
-	#Inspired by: https://stackoverflow.com/questions/6910641/how-do-i-get-indices-of-n-maximum-values-in-a-numpy-array
-	# idx_recommended_movieIds = np.argpartition(row, -n_recommendations)[-n_recommendations:]
-
-	# utils.get_movie_titles_from_ids(recommended_movieIds)
-
 	result = json.dumps(recommended_movieIds.tolist())
 	print(result)
 
 
 movieId = int(sys.argv[1])
-get_recommendations_for_movieId(movieId, 15)
+predict_similarity_matrix(movieId, 15)
 
 # n_total = 20
 # n_matches = 5
