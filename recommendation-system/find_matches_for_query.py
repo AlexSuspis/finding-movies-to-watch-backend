5f51c7c26204b9f7e53c9a9238c717abb6a0c136
Alex Suspiro <alexandresuspiro@gmail.com>
2022-07-01 14:11:55 +0100

    add pymongo[srv] to requirements.txt

--- recommendation-system/find_matches_for_query.py
+++ recommendation-system/find_matches_for_query.py
@@ -16,16 +16,16 @@ try:
 
     def find_movieIds_from_closest_titles_to(query, n):
         # movies_df = loader.load_processed_movies_locally()
-        movies_df = get_processed_movies_from_db()
+        movies_df = loader.get_processed_movies_from_db()
         # print(movies_df.head())
         # print(movies_df['title'])
         # print(movies_df['clean_title'])
 
-        clean_query = clean_string(query)
+        clean_query = utils.clean_string(query)
         # # print(clean_query)
 
         movies_df['similarity_to_query'] = movies_df['clean_title'].apply(
-            lambda clean_title: get_string_similarity(clean_title, clean_query))
+            lambda clean_title: utils.get_string_similarity(clean_title, clean_query))
         # print(movies_df['similarity_to_query'])
 
         # sort
